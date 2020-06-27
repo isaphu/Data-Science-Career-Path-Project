22f1be9fe2c5464887b9dbfa6c04e43ed84e970b
uronohan <uronohan@gmail.com>
2020-06-27 08:09:28 +0700

    Project #7 in function

--- Project#7 (FizzBuzz).py
+++ Project#7 (FizzBuzz).py
@@ -1,12 +1,18 @@
-for number in range(1,101):
-    if number % 3 == 0 & number % 5 == 0:
-        print('FizzBuzz')
-    elif number % 3 == 0:
-        print('Fizz')
-    elif number % 5 == 0:
-        print('Buzz')
+def fizz_buzz_or_not_fizz_buzz(number):
+    is_FizzBuzz = number % 3 == 0 & number % 5 == 0
+    is_Fizz = number % 3 == 0
+    is_Buzz = number % 5 == 0
+    if is_FizzBuzz:
+        return('FizzBuzz')
+    elif is_Fizz:
+        return('Fizz')
+    elif is_Buzz:
+        return('Buzz')
     else:
-        print(number)
+        return(number)
+
+for number in range(1, 101):
+    print(fizz_buzz_or_not_fizz_buzz(number))
 
 
 
