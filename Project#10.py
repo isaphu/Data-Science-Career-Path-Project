faefd49a1a6b0214257f5f4c5deb162862c15cf9
uronohan <uronohan@gmail.com>
2020-08-25 12:49:45 +0700

    adding project 10

--- Project#10.py
+++ Project#10.py
@@ -64,3 +64,24 @@ ax.set_xticklabels(['April', 'May', 'June', 'July', 'August', 'September'])
 plt.ylabel('Average Order Amount')
 plt.title('Order Amount over Time')
 plt.show()
+
+
+#Customer Types
+
+import codecademylib
+from matplotlib import pyplot as plt
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+
+customer_amount = orders.groupby('customer_id').price.sum().reset_index()
+
+print customer_amount.head()
+
+plt.hist(customer_amount.price.values,
+        range=(0, 200), bins=40)
+plt.xlabel('Total Spent')
+plt.ylabel("Number of Customers")
+plt.title('Customer Expenditure Over 6 Months')
+
+plt.show()
